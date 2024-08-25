5c5f18800a3afec48a23f25d61e3ffcc65fcfb54
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-08-25 15:05:27 +0400

    Update AddDriver.js

--- app/cars/AddDriver.js
+++ app/cars/AddDriver.js
@@ -45,7 +45,7 @@ const deleteDriver=async(jobId)=>{
 }
   return (
     <>
-      <Button  onClick={() => setOpenModal(true)}  className="h-7 w-7  flex justify-center items-center rounded-full " color={car.match_found
+      <Button    className="h-7 w-7  flex justify-center items-center rounded-full " color={car.match_found
 ?"success":"warning"}>{car.nida}</Button>
    
       <Modal show={openModal} onClose={() => setOpenModal(false)}>
