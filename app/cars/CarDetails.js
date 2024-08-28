0f9b35d6ada855d74c39a215174f92fa0d2ab49a
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-08-28 18:12:17 +0400

    Update CarDetails.js

--- app/cars/CarDetails.js
+++ app/cars/CarDetails.js
@@ -20,7 +20,7 @@ export default function CarDetails({car}) {
    }
   return (
     <>
-      <Tooltip content="تفاصيل الدورية">
+      <Tooltip content="عرض">
 
 <svg  onClick={() => setOpenModal(true)} className='w-6 h-6 fill-gray-600' stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 20 20" aria-hidden="true" height="200px" width="200px" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
 </Tooltip>
