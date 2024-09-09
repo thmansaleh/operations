696276d6e0a69520dcca3db667b043c29063ab3e
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-09-09 18:57:34 +0400

    Update Carousel.js

--- app/reports/report-details/Carousel.js
+++ app/reports/report-details/Carousel.js
@@ -5,14 +5,14 @@ import { swrReportImages } from '@/app/swr/reportImages'
 export function Component() {
     const { data , error, isLoading,mutate } = swrReportImages()
 
-  return (
+  if(data) return (
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
       <Carousel slideInterval={5000}>
-        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+{data.map(image=>{
+
+return <img src={image.url} alt="..." />
+})}
+        
       </Carousel>
     </div>
   );
